db08ace2a3a279a7c57913dd0babf506d7e110a0
Super Popcorn <vtkkllx2367@gmail.com>
2023-09-21 18:51:27 +0300

    responsive CustomRouterLink

--- src/components/UI/CustomRouterLink.vue
+++ src/components/UI/CustomRouterLink.vue
@@ -2,13 +2,13 @@
   <router-link class="cus-router-link" :to="to">
     <slot> </slot>
     <div class="arrow-button-svg-wrap">
-        <arrow-button-svg></arrow-button-svg>
+      <arrow-button-svg></arrow-button-svg>
     </div>
   </router-link>
 </template>
 
 <script>
-import ArrowButtonSvg from '@/svg/ArrowButtonSvg.vue';
+import ArrowButtonSvg from "@/svg/ArrowButtonSvg.vue";
 export default {
   components: { ArrowButtonSvg },
   name: "custom-router-link",
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/variables';
+@import "@/assets/variables";
 .cus-router-link {
   font-size: $font-sx-m;
   font-weight: 400;
@@ -27,13 +27,30 @@ export default {
   color: $dark-text;
   display: flex;
   flex-direction: row;
+  justify-content: center;
   align-items: center;
 }
 
 .arrow-button-svg-wrap {
-    width: 32px;
-    height: 32px;
-    margin-left: 9px;
+  width: 32px;
+  height: 32px;
+  margin-left: 9px;
 }
 
+@media (max-width: 700px) {
+  .cus-router-link {
+    font-size: $font-sx-m - 5;
+  }
+  .arrow-button-svg-wrap {
+    width: 25px;
+    height: 25px;
+  }
+}
+
+@media (max-width: 400px) {
+  .cus-router-link {
+    font-size: $font-sx-m - 8;
+  }
+
+}
 </style>
